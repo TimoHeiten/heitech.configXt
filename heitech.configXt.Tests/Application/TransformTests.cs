31b4ecbceedac1d805643aae3a2b1a1024de995d
timo.heiten <timoheiten@googlemail.com>
2020-06-22 15:52:17 +0200

    condensed jsontransform

--- heitech.configXt.Tests/Application/TransformTests.cs
+++ heitech.configXt.Tests/Application/TransformTests.cs
@@ -94,95 +94,103 @@ namespace heitech.configXt.Tests.Application
             Assertion(collection, "Simple", "Value", 0);
             Assertion(collection, "RabbitMQ:Host", "localhost", 1);
             Assertion(collection, "RabbitMQ:Port", "5672", 2);
-            Assertion(collection, "Connections", "1,2,3,4", 3);
+            Assertion(collection, "Connections:0", "1", 3);
+            Assertion(collection, "Connections:1", "2", 4);
+            Assertion(collection, "Connections:2", "3", 5);
+            Assertion(collection, "Connections:3", "4", 6);
         }
 
-        // [Fact]
-        // public void NestedObjectAndArray()
-        // {
-        //     string json = @"
-        //     {
-        //         ""Object"" : {
-        //             ""Nested"" : {
-        //                 ""Array"" : [
-        //                     1,2,3,4
-        //                 ],
-        //                 ""Key"" : ""Value""
-        //             },
-        //             ""Nested2"" : {
-        //                 ""Nested3"" : {
-        //                     ""N-Key"" : ""N-Value""
-        //                 }
-        //             }
-        //         }
-        //     }
-        //     ";
+        [Fact]
+        public void NestedObjectAndArray()
+        {
+            string json = @"
+            {
+                ""Object"" : {
+                    ""Nested"" : {
+                        ""Array"" : [
+                            1,2,3,4
+                        ],
+                        ""Key"" : ""Value""
+                    },
+                    ""Nested2"" : {
+                        ""Nested3"" : {
+                            ""N-Key"" : ""N-Value""
+                        }
+                    }
+                }
+            }
+            ";
 
-        //     var transform = new JsonTransform(_model);
+            var transform = new JsonTransform();
 
-        //     var result = transform.Parse(json);
-        //     var collection = (result.Result as ConfigCollection).WrappedConfigEntities;
+            var result = transform.Parse(json);
+            var collection = (result.Result as ConfigCollection).WrappedConfigEntities;
 
-        //     Assertion(collection, "Object:Nested:Array", "1,2,3,4", 0);
-        //     Assertion(collection, "Object:Nested:Key", "Value", 1);
-        //     Assertion(collection, "Object:Nested2:Nested3:N-Key", "N-Value", 2);
-        // }
+            Assertion(collection, "Object:Nested:Array:0", "1", 0);
+            Assertion(collection, "Object:Nested:Array:1", "2", 1);
+            Assertion(collection, "Object:Nested:Array:2", "3", 2);
+            Assertion(collection, "Object:Nested:Array:3", "4", 3);
+            Assertion(collection, "Object:Nested:Key", "Value", 4);
+            Assertion(collection, "Object:Nested2:Nested3:N-Key", "N-Value", 5);
+        }
 
-        // [Fact]
-        // public void ArrayWithObjects()
-        // {
-        //     // todo- not yet allowed
-        //     string json = @"
-        //     {
-        //         ""Array"" : [
-        //             {
-        //                 ""Key-1"" : ""Value-1"",
-        //             },
-        //             {
-        //                 ""Key-2"" : ""Value-2"",
-        //             },
-        //             42
-        //         ]
-        //     }
-        //     ";
+        [Fact]
+        public void ArrayWithObjects()
+        {
+            string json = @"
+            {
+                ""Array"" : [
+                    {
+                        ""Key-1"" : ""Value-1"",
+                    },
+                    {
+                        ""Key-2"" : ""Value-2"",
+                    },
+                    42
+                ]
+            }
+            ";
 
-        //     var transform = new JsonTransform(_model);
+            var transform = new JsonTransform();
 
-        //     var result = transform.Parse(json);
-        //     var collection = (result.Result as ConfigCollection).WrappedConfigEntities;
+            var result = transform.Parse(json);
+            var collection = (result.Result as ConfigCollection).WrappedConfigEntities;
 
-        //     Assertion(collection, "Array:Key-1", "Value-1", 0);
-        //     Assertion(collection, "Array:Key-2", "Value-2", 1);
-        // }
+            Assertion(collection, "Array:0:Key-1", "Value-1", 0);
+            Assertion(collection, "Array:1:Key-2", "Value-2", 1);
+        }
 
-        // [Fact]
-        // public void ArrayWithObjectWithArray()
-        // {
-        //     string json = @"
-        //     {
-        //         ""Array"" : [
-        //             {
-        //                 ""Key-1"" : [
-        //                         1, 2, 3, 4
-        //                     ],
-        //             },
-        //             {
-        //                 ""Nested"" : {
-        //                         ""NestedKey"" : ""NestedValue""
-        //                     }
-        //             }
-        //         ]
-        //     }
-        //     ";
+        [Fact]
+        public void ArrayWithObjectWithArray()
+        {
+            string json = @"
+            {
+                ""Array"" : [
+                    {
+                        ""Key-1"" : [
+                                1, 2, 3, 4
+                            ],
+                    },
+                    {
+                        ""Nested"" : {
+                                ""NestedKey"" : ""NestedValue""
+                            }
+                    }
+                ]
+            }
+            ";
 
-        //     var transform = new JsonTransform(_model);
+            var transform = new JsonTransform();
 
-        //     var result = transform.Parse(json);
-        //     var collection = (result.Result as ConfigCollection).WrappedConfigEntities;
+            var result = transform.Parse(json);
+            var collection = (result.Result as ConfigCollection).WrappedConfigEntities;
 
-        //     Assertion(collection, "Array:Key-1", "1,2,3,4", 0);
-        //     Assertion(collection, "Array:Nested:NestedKey", "NestedValue", 1);
-        // }
+            Assertion(collection, "Array:0:Key-1:0", "1", 0);
+            Assertion(collection, "Array:0:Key-1:1", "2", 1);
+            Assertion(collection, "Array:0:Key-1:2", "3", 2);
+            Assertion(collection, "Array:0:Key-1:3", "4", 3);
+            Assertion(collection, "Array:1:Nested:NestedKey", "NestedValue", 4);
+        }
 
         private void Assertion(IEnumerable<ConfigEntity> collection, string key, string value, int at)
         {
