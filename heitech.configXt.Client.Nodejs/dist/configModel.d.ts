76f49fd0dadf055c213398738552714b74df544b
timo.heiten <timoheiten@googlemail.com>
2020-07-05 12:17:12 +0200

    ignoring dist folder

--- heitech.configXt.Client.Nodejs/dist/configModel.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class ConfigurationModel {
-    Name: string;
-    Value: string;
-    constructor(Name: string, Value: string);
-}
--- heitech.configXt.Client.Nodejs/dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { AuthModel } from './authModel';
-export declare function selectUseCase(appName: string, user: AuthModel): void;
